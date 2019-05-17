95e9e2b8e4113ce6ceb256470144c346cc22ce61
Merih <merimemax@gmail.com>
2019-05-17 13:53:45 -0400

    review-form

---
+++ gMDB-UI/src/app/review-form/review-form.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Review } from '../review/review';
+
+@Component({
+  selector: 'app-review-form',
+  templateUrl: './review-form.component.html',
+  styleUrls: ['./review-form.component.css']
+})
+export class ReviewFormComponent implements OnInit {
+  reviewform: FormGroup;
+  constructor(private formbuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.reviewform = this.formbuilder.group({
+      reviewtext:[''],
+      reviewtitle:['']
+      
+    })
+  }
+  review(){
+    console.log("review");
+    const review = new Review();
+    review.reviewText = this.reviewform.value.reviewtext;
+    review.reviewTitle = this.reviewform.value.reviewtitle;
+    console.log(review);
+
+  }
+}
